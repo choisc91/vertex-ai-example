71f240c2aa78889ce307622586eb366aa431a277
Characin.choi <choisc91@naver.com>
2025-06-09 11:38:57 +0900

    Add tests for Message creation

--- app/src/test/java/kr/co/characin/gemini_api_example/ExampleUnitTest.kt
+++ app/src/test/java/kr/co/characin/gemini_api_example/ExampleUnitTest.kt
@@ -1,8 +1,9 @@
 package kr.co.characin.gemini_api_example
 
 import org.junit.Test
-
 import org.junit.Assert.*
+
+import kr.co.characin.gemini_api_example.Message
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +14,25 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun prompt_returnsPromptMessage() {
+        val text = "prompt text"
+        val message = Message.prompt(text, null)
+
+        assertEquals(Message.TYPE_PROMPT, message.type)
+        assertEquals(text, message.text)
+        assertNull(message.bitmap)
+    }
+
+    @Test
+    fun response_returnsResponseMessage() {
+        val text = "response text"
+        val message = Message.response(text, null)
+
+        assertEquals(Message.TYPE_RESPONSE, message.type)
+        assertEquals(text, message.text)
+        assertNull(message.bitmap)
     }
 }
